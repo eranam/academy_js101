1e994eec174c528ca0edd707051132e542f9a1c7
eranam <Eran Amar>
2014-07-17 16:58:24 +0300

    complete the EventEmitter & ApproxPi exercises.

--- src/callbacks.js
+++ src/callbacks.js
@@ -1,13 +1,97 @@
-function EventEmitter(){
-    this.listeners = {};
+'use strict';
+
+function EventEmitter() {
+    this.recurringListeners = {};
+    this.onceListeners = {}
 }
 
-EventEmitter.prototype.emit = function emit(eventName){
-    var callbacks = this.listeners[eventName];
-    arguments.shift(); // remove the eventName from the argument list
-    if (callbacks){
-        for(var i=0; i<callbacks.length; i++){
-
-        }
+function addCallbacksToDest(dest, eventName, func) {
+    if (eventName in dest) {
+        dest[eventName].push(func);
+    } else {
+        dest[eventName] = [func];
     }
+}
+
+EventEmitter.prototype.addListener = function addListener(eventName, func) {
+    addCallbacksToDest(this.recurringListeners, eventName, func);
 };
+
+EventEmitter.prototype.once = function once(eventName, func) {
+    addCallbacksToDest(this.onceListeners, eventName, func);
+};
+
+EventEmitter.prototype.emit = function emit(eventName, data) {
+    var that = this;
+
+    function callbacksRunner(callbacks) {
+        var funcArr = (callbacks || []).slice();
+        funcArr.forEach(function invokator(func) {
+            func.call(that, data);
+        })
+    }
+
+    callbacksRunner(that.onceListeners[eventName]);
+    callbacksRunner(that.recurringListeners[eventName]);
+    delete that.onceListeners[eventName];
+};
+
+function removeElementFromArray(arr, element) {
+    if (arr) {
+        var indx = arr.indexOf(element);
+        delete arr[indx];
+    }
+}
+
+EventEmitter.prototype.removeListener = function removeListener(eventName, func) {
+    removeElementFromArray(this.recurringListeners[eventName], func);
+    removeElementFromArray(this.onceListeners[eventName], func);
+};
+
+function ApproxyPi() {
+    this.pi = 0;
+    this.percision = 0;
+    this.iterationNum = 0;
+}
+
+ApproxyPi.prototype = new EventEmitter();
+ApproxyPi.prototype.constructor = ApproxyPi;
+
+ApproxyPi.getSeriesItem = function getSeriesItem(itemIndex) {
+    return 4.0 * Math.pow(-1, itemIndex) / (1 + 2 * itemIndex);
+};
+
+ApproxyPi.prototype.calc = function calcPi(precisionDigitIndex) {
+    var that = this;
+    var maxPrecision = Math.pow(10, -precisionDigitIndex);
+
+    function calcSingleBulk() {
+        var increment,
+            currPrecision = Math.pow(10, -that.percision),
+            absIncrement = 1;
+
+        for (var i = 0; i < 1000 && absIncrement >= maxPrecision; i++) {
+            increment = ApproxyPi.getSeriesItem(that.iterationNum);
+            that.pi += increment;
+            that.iterationNum++;
+
+            absIncrement = Math.abs(increment);
+            while (absIncrement < currPrecision) {
+                that.percision++;
+                currPrecision = Math.pow(10, -that.percision);
+            }
+        }
+
+        if (that.percision < precisionDigitIndex) {
+            that.emit('progress');
+            setTimeout(calcSingleBulk, 1);
+        } else {
+            that.emit('done');
+        }
+
+    }
+
+    this.emit('start');
+    calcSingleBulk();
+};
+
